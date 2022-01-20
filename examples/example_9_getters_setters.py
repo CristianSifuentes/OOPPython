e0243e803ce01254130aef459f824c1b33774b50
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-01-20 15:33:06 -0600

    Advanced example for getters and setters in python

---
+++ examples/example_9_getters_setters.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Condition(Enum):
+    NEW = 0
+    GOOD = 1
+    OKAY = 2
+    BAD = 3
+
+
+class MethodNotAllowed(Exception):
+    pass
+
+
+class Bike(object):
+    pass
+
+if __name__ == '__main__':
+    pass
