a655f2014d5cd7348a8f4016808cef8a8f8e2e18
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-01-21 14:43:18 -0600

    Creating new example for refactoring code

---
+++ refactoring/refactoring_1_procedural.py
@@ -0,0 +1,15 @@
+"""
+Simulate a simple board game
+There are 2 players.
+Each player takes turn rolling a die and moving that number of spaces.
+The first person to spacd 100 wins.
+
+"""
+from random import  randint
+
+
+def play_game():
+    pass
+
+if __name__ == '__main__':
+    play_game()
