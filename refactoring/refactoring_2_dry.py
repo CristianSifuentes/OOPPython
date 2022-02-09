0f3107403207f8dac68a89692f912b695bcaefaa
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-02-09 11:39:05 -0600

    A Game simulator

---
+++ refactoring/refactoring_2_dry.py
@@ -0,0 +1,17 @@
+"""
+Simulate a simple board game.
+There are 2 players.
+Each player takes turn rolling a die and moving that number of spaces.
+There first person to space 100 wins.
+"""
+from random import randint
+
+def run_game(num_player=2):
+    pass
+
+if __name__ == '__main__':
+    print("-- GAME 1 --")
+    run_game(4)
+    print("-- GAME 2 --")
+    run_game(3)   
+   
