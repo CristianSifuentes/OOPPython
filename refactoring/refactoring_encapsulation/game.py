7c3e8f900ed49d9678105a196db645115a7f1680
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-02-09 17:07:43 -0600

    Encapsulation example in python using refactoring

---
+++ refactoring/refactoring_encapsulation/game.py
@@ -0,0 +1,31 @@
+from player import Player
+
+class Game:
+    counter = 0
+
+    def __init__(self, num_players, target_score=100):
+        Game.counter += 1
+        self.game_num = Game.counter
+        self.players = [Player(i + 1) for i in range(num_players)]
+        self.target_score = target_score
+
+    def player_has_won(self, player):
+        return player.score >= self.target_score
+
+    def run(self):
+        print(f"----- {self} start ------")
+
+        while True:
+            for player in self.players:
+                player.make_move()
+
+                if self.player_has_won(player):
+                    print(f"{player} wins!")
+                    print(f"------ {self} end --------")
+                    return      
+
+    def __str__(self):
+        return f"Game {self.game_num}"
+
+    def __repr__(self):
+        return f"Game({len(self.players)})"
---
+++ refactoring/refactoring_encapsulation/player.py
@@ -0,0 +1,25 @@
+from random import randint
+
+class Player:
+    def __init__(self, player_num):
+        self._score = 0
+        self.num = player_num
+
+    @staticmethod
+    def _roll():
+        return randint(1, 6)
+
+    @property
+    def score(self):
+        return self._score
+
+    def make_move(self):
+        roll = self._roll()
+        self._score += roll
+        print(f"{self} score: {self.score} (rolled a {roll})")
+
+    def __str__(self):
+        return f"Player {self.num}"
+
+    def __repr__(self):
+        return f"Player({self.num})"
